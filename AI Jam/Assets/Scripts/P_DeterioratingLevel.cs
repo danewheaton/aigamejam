9dd9989bab00ab0e4f3b820c84435b5abd534f13
Dane Wheaton <wheaton.dane@gmail.com>
2017-07-22 13:45:27 -0500

    finished disappearing objects mechanic

--- AI Jam/Assets/Scripts/P_DeterioratingLevel.cs
+++ AI Jam/Assets/Scripts/P_DeterioratingLevel.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 public class P_DeterioratingLevel : MonoBehaviour
 {
     [SerializeField] float visionCone = 90;
-    List<GameObject> objectsToDisappear;
+    List<GameObject> objectsToDisappear = new List<GameObject>();
+    bool objectsCanDisappear;
 
     private void Start()
     {
@@ -15,13 +16,19 @@ public class P_DeterioratingLevel : MonoBehaviour
 
     private void Update()
     {
-        foreach (GameObject g in objectsToDisappear)
+        if (Input.GetKeyDown(KeyCode.Space)) objectsCanDisappear = !objectsCanDisappear;
+
+        if (objectsCanDisappear)
         {
-            Vector3 targetDirection = transform.position - g.transform.position;
-            if (Vector3.Angle(transform.forward, targetDirection) < visionCone)
+            foreach (GameObject g in objectsToDisappear)
             {
-                g.SetActive(false);
+                Vector3 targetDirection = transform.position - g.transform.position;
+                if (Vector3.Angle(transform.forward, targetDirection) < visionCone)
+                {
+                    g.SetActive(false);
+                }
             }
         }
+        
     }
 }
