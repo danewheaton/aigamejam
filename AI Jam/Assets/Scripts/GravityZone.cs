f79e9dc2c4022b5edafa3424cfad585d4e796609
Dane Wheaton <wheaton.dane@gmail.com>
2017-07-22 12:00:21 -0500

    non-ufps player controller

---
+++ AI Jam/Assets/Scripts/GravityZone.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") Physics.gravity = new Vector3(0, 9.81f, 0);
+    }
+}
